118387deeae2a3830e177eb8ca3ce4656f000140
DELL <DELL@DESKTOP-RJ7LTOA>
2020-10-15 13:35:19 +0530

    add department

---
+++ EmployeeManagement/src/com/lti/model/department.java
@@ -0,0 +1,19 @@
+package com.lti.model;
+
+public class department {
+     int departId;
+     String DepartName;
+	public int getDepartId() {
+		return departId;
+	}
+	public void setDepartId(int departId) {
+		this.departId = departId;
+	}
+	public String getDepartName() {
+		return DepartName;
+	}
+	public void setDepartName(String departName) {
+		DepartName = departName;
+	}
+     
+}
